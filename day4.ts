59a27755f89e00cb34d669fab20505b1e7774e3b
cbob <jaakkoko@gmail.com>
2020-12-04 17:13:59 +0200

    day4

--- day4.ts
+++ day4.ts
@@ -4,6 +4,10 @@ const file = fs.readFileSync('input/day4.txt', 'utf-8');
 const input = file.split("\r\n\r\n");
 
 const reqFields: string[] = [ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" ];
+
+const hcl = new RegExp(/^#[0-9a-f]{6}$/);
+const pid = new RegExp(/^\d{9}$/);
+
 var valids = 0;
 
 input.forEach(element => {
@@ -12,8 +16,43 @@ input.forEach(element => {
     reqFields.forEach(reqField => {
         if (!line.includes(reqField)) valid = false;
     });
-    if (valid) valids++;
-});
 
+    if (valid) {
+        const fields: any = {}
+        line.split(" ").forEach(field => {
+          const [k, v] = field.split(":");
+          fields[k] = v == undefined ? "" : v.trim();
+        });
+
+        if (valid) {
+          const hgtValue = fields["hgt"];
+          const hgtUnit = hgtValue.substring(hgtValue.length - 2);
+          const hgt = hgtValue.substring(0, hgtValue.length - 2);
+
+          valid = hgtUnit === "cm" || hgtUnit === "in";
+
+          if (valid) {
+            if (hgtUnit === "cm" && (+hgt < 150 || +hgt > 193)) {
+              valid = false;
+            }
+            else if (hgtUnit === "in" && (+hgt < 59 || +hgt > 76)) {
+              valid = false;
+            }
+          }
+        }
+
+        valid =
+          (+fields["byr"] >= 1920 && +fields["byr"] <= 2002) &&
+          (+fields["iyr"] >= 2010 && +fields["iyr"] <= 2020) &&
+          (+fields["eyr"] >= 2020 && +fields["eyr"] <= 2030) &&
+          [ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" ].indexOf(fields["ecl"]) !== -1 &&
+          null !== fields["pid"].match(pid) &&
+          null !== fields["hcl"].match(hcl);
+
+        if (valid) {
+          valids++;
+        }
+    }
+});
 
 console.log(valids);
