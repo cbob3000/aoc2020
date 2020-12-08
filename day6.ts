436dfbf60d3d800c11313b0f64de20ab865b29f8
Jaska <jaakkoko@gmail.com>
2020-12-08 20:20:27 +0200

    day8

--- day6.ts
+++ day6.ts
@@ -2,7 +2,17 @@ import fs = require("fs");
 
 const file = fs.readFileSync('input/day6.txt', 'utf-8');
 const input = file.split("\n\r");
+const char = new RegExp(/[a-z]/);
 
+var yesses = 0;
 input.forEach(group => {
-    console.log(group);
+    const answers: Set<string> = new Set();
+    for (var i = 0; i < group.length; i++) {
+        if (group[i].match(char) !== null) {
+            answers.add(group[i]);
+        }
+    }
+    yesses += answers.size;
 });
+
+console.log(yesses);
---
+++ day8.ts
@@ -0,0 +1,4 @@
+import fs = require("fs");
+
+const file = fs.readFileSync('input/day8.txt', 'utf-8');
+const input = file.split("\n");
